1e6195109a8beeff595904bc96272eec21f2c5bc
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-05-10 18:37:56 +0300

    refactor:  add `menuItem` section

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,7 +30,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="
           p-4
           md:py-1
@@ -52,6 +53,29 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+          absolute 
+          rounded-xl 
+          shadow-md
+          w-[40vw]
+          md:w-3/4 
+        bg-white 
+          overflow-hidden 
+          right-0 
+          top-12 
+          text-sm
+      "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign Up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
