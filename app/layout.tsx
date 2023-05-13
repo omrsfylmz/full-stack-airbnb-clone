88070ac747919dc8a5f5b334d376520b6f153ca1
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-05-13 22:18:45 +0300

    fix: add static register modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,11 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
-  title: "Airbnbn",
+  title: "Airbnb",
   description: "Airbnb clone",
 };
 
@@ -23,7 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen />
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
