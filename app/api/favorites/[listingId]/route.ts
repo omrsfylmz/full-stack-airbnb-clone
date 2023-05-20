fb97df4689550a1ad51ccf7ea24f8c386ec94026
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-05-20 12:24:06 +0300

    fix: add extra fields for favoriting

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -7,6 +7,35 @@ interface IParams {
   listingId?: string;
 }
 
+export async function POST(request: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
 export async function DELETE(
   request: Request,
   { params }: { params: IParams }
@@ -23,12 +52,18 @@ export async function DELETE(
     throw new Error("Invalid ID");
   }
 
-  const listing = await prisma.listing.deleteMany({
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
     where: {
-      id: listingId,
-      userId: currentUser.id,
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
     },
   });
 
-  return NextResponse.json(listing);
+  return NextResponse.json(user);
 }
